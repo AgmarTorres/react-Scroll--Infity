98924afdee9442e092edd5f0cfac6ce4bf9b144f
Agmar Torres <agmartorres@gmail.com>
2020-02-19 08:25:31 -0300

    adding Infinity Scroll

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const style = {
+  height: 30,
+  border: "1px solid green",
+  margin: 6,
+  padding: 8
+};
+
+class App extends React.Component {
+  state = {
+    repositories:[],
+    items: [],
+    page: 1,
+    hasMore: true
+  };
+
+  componentDidMount(){
+    fetch(`https://api.github.com/search/repositories?q=language:Javascript&sort=stars&page=${this.state.page}`)
+    .then(response => response.json())
+    .then(
+      items => this.setState({repositories : items, items : items.items  },console.log(this.state.items.items))
+    
+      )
+  }
+
+  fetchMoreData = () => {
+    this.setState({page: this.state.page + 1})
+    console.log(this.state.page)
+    setTimeout(() => {  
+      fetch(`https://api.github.com/search/repositories?q=language:Javascript&sort=stars&page=${this.state.page}`)
+      .then(response => response.json())
+      .then(
+        items => this.setState({items: this.state.items.concat(items.items) })
+      )
+    }, 500);
+  };
+
+  render() {
+    console.log(this.state.repositories)
+    return (
+      <div>
+        <h1>demo: react-infinite-scroll-component</h1>
+        <hr />
+        {
+          this.state.items.length ?
+          <InfiniteScroll
+            dataLength={this.state.items.length}
+            next={this.fetchMoreData}
+            hasMore={this.state.hasMore}
+            loader={<h4>Loading...</h4>}
+            endMessage={
+              <p style={{ textAlign: "center" }}>
+                <b>Yay! You have seen it all</b>
+              </p>
+            }
+          >
+            >
+            {this.state.items.map((i, index) => (
+              <div style={style} key={index}>
+                div - #{index}
+              </div>
+            ))}
+          </InfiniteScroll>
+          :
+          null
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
