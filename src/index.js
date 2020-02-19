eb35b37b64cdb70fa0d0a6d2e7430538721c8f57
Agmar Torres <agmartorres@gmail.com>
2020-02-19 08:33:52 -0300

    upgrade scroll

--- src/index.js
+++ src/index.js
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider} from 'react-redux' 
-import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-    <Provider>
-        <BrowserRouter>
             <App />
-        </BrowserRouter>    
         , document.getElementById('root')        
-    </Provider>
+    
 );
 
 
